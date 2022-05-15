023115dcc23e6a1ba606c716a9dc55c2c35cd0ec
azizAYDI <86956679+azizAYDI@users.noreply.github.com>
2022-05-15 21:51:55 +0200

    initial commit

---
+++ text Editor.js
@@ -0,0 +1,258 @@
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButtons = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+//List of fontlist
+let fontList = [
+  "Arial",
+  "Verdana",
+  "Times New Roman",
+  "Garamond",
+  "Georgia",
+  "Courier New",
+  "cursive",
+];
+
+
+
+//Initial Settings
+const initializer = () => {
+    //function calls for highlighting buttons
+    //No highlights for link, unlink,lists, undo,redo since they are one time operations
+    highlighter(alignButtons, true);
+    highlighter(spacingButtons, true);
+    highlighter(formatButtons, false);
+    highlighter(scriptButtons, true);
+    //create options for font names
+    fontList.map((value) => {
+      let option = document.createElement("option");
+      option.value = value;
+      option.innerHTML = value;
+      fontName.appendChild(option);
+    });
+    //fontSize allows only till 7
+    for (let i = 1; i <= 7; i++) {
+      let option = document.createElement("option");
+      option.value = i;
+      option.innerHTML = i;
+      fontSizeRef.appendChild(option);
+    }
+    //default size
+    fontSizeRef.value = 3;
+  };
+
+
+  
+  //main logic
+  const modifyText = (command, defaultUi, value) => {
+    //execCommand executes command on selected text
+    document.execCommand(command, defaultUi, value);
+  };
+  //For basic operations which don't need value parameter
+  optionsButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      modifyText(button.id, false, null);
+    });
+  });
+  //options that require value parameter (e.g colors, fonts)
+  advancedOptionButton.forEach((button) => {
+    button.addEventListener("change", () => {
+      modifyText(button.id, false, button.value);
+    });
+  });
+  //link
+  linkButton.addEventListener("click", () => {
+    let userLink = prompt("Enter a URL");
+    //if link has http then pass directly else add https
+    if (/http/i.test(userLink)) {
+      modifyText(linkButton.id, false, userLink);
+    } else {
+      userLink = "http://" + userLink;
+      modifyText(linkButton.id, false, userLink);
+    }
+  });
+  //Highlight clicked button
+  const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+      button.addEventListener("click", () => {
+        //needsRemoval = true means only one button should be highlight and other would be normal
+        if (needsRemoval) {
+          let alreadyActive = false;
+          //If currently clicked button is already active
+          if (button.classList.contains("active")) {
+            alreadyActive = true;
+          }
+          //Remove highlight from other buttons
+          highlighterRemover(className);
+          if (!alreadyActive) {
+            //highlight clicked button
+            button.classList.add("active");
+          }
+        } else {
+          //if other buttons can be highlighted
+          button.classList.toggle("active");
+        }
+      });
+    });
+  };
+  const highlighterRemover = (className) => {
+    className.forEach((button) => {
+      button.classList.remove("active");
+    });
+  };
+  window.onload = initializer();
+
+
+
+
+const editor = new EditorJS({
+    holder: 'text-input',
+    tools: { 
+        header: {
+          class: Header, 
+          inlineToolbar: ['link'] 
+        }, 
+        list: { 
+          class: List, 
+          inlineToolbar:  ['link','bold']  
+        } ,
+        image: {
+            class: SimpleImage,
+            inlineToolbar: ['link'],
+          },
+
+          checklist: {
+            class: Checklist,
+            inlineToolbar: true,
+          },
+          quote: {
+            class: Quote,
+            inlineToolbar: true,
+            config: {
+              quotePlaceholder: 'Enter a quote',
+              captionPlaceholder: 'Quote\'s author',
+            },
+            shortcut: 'CMD+SHIFT+O'
+          },
+          warning: Warning,
+          marker: {
+            class:  Marker,
+            shortcut: 'CMD+SHIFT+M'
+          },
+          code: {
+            class:  CodeTool,
+            shortcut: 'CMD+SHIFT+C'
+          },
+          delimiter: Delimiter,
+          inlineCode: {
+            class: InlineCode,
+            shortcut: 'CMD+SHIFT+C'
+          },
+          linkTool: LinkTool,
+
+
+          embed:{
+            class:  Embed,
+            inlineToolbar: false,
+            config: {
+                services:{
+                    youtube:true,
+                    coub:true,
+                }
+            }
+          } ,
+
+
+
+
+          table: {
+            class: Table,
+            inlineToolbar: true,
+            shortcut: 'CMD+ALT+T'
+          },
+
+
+          paragraph:{
+            class: Paragraph,
+            inlineToolbar: true,
+          },
+
+          underline:{
+            class: Underline,
+            inlineToolbar: true,
+          },
+
+
+          personality:{
+            class: Personality,
+            inlineToolbar: true,
+          },
+
+          list:{
+            class: NestedList,
+            inlineToolbar: true,
+          },
+
+  
+
+
+
+
+
+
+
+
+      }, 
+
+});
+
+
+
+
+
+  $(function () {
+    $("#main").on("click", function () {
+
+
+      editor.save().then((savedData) => {
+       
+
+
+
+
+    }).catch((error)=>{
+
+        console.log(error);
+    } );
+
+
+
+
+
+
+
+
+
+
+
+
+      setTimeout(function () {
+        $("#lnch").addClass("launching").text("SAVING");
+        $("#lnch_btn").addClass("launching");
+      }, 0);
+  
+      setTimeout(function () {
+        $("#lnch").addClass("launched").text("SAVED");
+        $("#lnch_btn").addClass("launched");
+      }, 1500);
+    });
+  });
+
+
+  
